0ecf915a28dbcee3da1a20a7b4e4537c9331ab45
Florent Pignaud <florent.pignaud@gmail.com>
2017-01-12 15:42:10 -0500

    Beginning of CRUD update

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   root 'welcome#home'
 
   get 'about', to: 'welcome#about'
+
+
+  resources :articles
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
